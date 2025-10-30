2cf15823e40c32baad6c2991cac866e6ca55fc60
Dani Vigarani <117960498+danielavigarani@users.noreply.github.com>
2025-10-30 22:41:16 +0000

    resolucaovariaveis

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,18 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
+nome = 'Daniela'
+profissao = "programadora"
+objetivo = '''
+Aprender programação
+e me tornar uma
+desenvolvedora fullstack'''
 
 # Declare variáveis de cada tipo numérico
+numero_inteiro = 21
+valor = 2525.00
 
 
 # Declare uma variável do tipo booleano
+verdadeiro = True
+falso = False
